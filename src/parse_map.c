8d773403547ac871371c99e02620811a257f1a30
42yasuke <j.ralph1998@gmail.com>
2023-06-09 21:23:26 +0200

    ft_map_closed finished

--- src/parse_map.c
+++ src/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: jose <jose@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 19:51:12 by jose              #+#    #+#             */
-/*   Updated: 2023/06/09 20:40:30 by jose             ###   ########.fr       */
+/*   Updated: 2023/06/09 21:21:36 by jose             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,22 +51,49 @@ static int	ft_verif_first_and_last_char_of_each_line(char **file, int i_start)
 	return (true);
 }
 
+static int	ft_verif_space_around_him(char **file, int i, int j)
+{
+	if (file[i - 1][j - 1] == ' ' || file[i - 1][j] == ' ' || \
+	file[i - 1][j + 1] == ' ' || file[i][j - 1] == ' ' || \
+	file[i][j] == ' ' || file[i][j + 1] == ' ' || \
+	file[i + 1][j - 1] == ' ' || file[i + 1][j] == ' ' || \
+	file[i + 1][j + 1] == ' ')
+		return (false);
+	return (true);
+}
+
+static int	ft_verif_all_0_and_the_player(char **file, int i_start, int i_end)
+{
+	int	i;
+	int	j;
+
+	i = i_start;
+	while (++i != i_end)
+	{
+		j = -1;
+		while (file[i][++j])
+		{
+			if (file[i][j] == '0' || file[i][j] == 'N' || file[i][j] == 'S' || \
+			file[i][j] == 'E' || file[i][j] == 'W')
+			{
+				if (!ft_verif_space_around_him(file, i, j))
+					return (false);
+			}
+		}
+	}
+	return (true);
+}
+
 int	ft_map_closed(char **file, int i)
 {
-	int	j;
 	int	i_start;
-	int	i_end;
 
-	j = -1;
 	i_start = i;
 	while (file[i])
 		i++;
-	i_end = i - 1;
-	if (!ft_verif_first_and_last_lines(file, i_start, i_end))
+	if (!ft_verif_first_and_last_lines(file, i_start, i - 1))
 		return (false);
 	if (!ft_verif_first_and_last_char_of_each_line(file, i_start))
 		return (false);
-	if (!ft_verif_all_0_and_the_player(file, i_start))
-		retunr (false);
-	return (true);
+	return (ft_verif_all_0_and_the_player(file, i_start, i -1));
 }
